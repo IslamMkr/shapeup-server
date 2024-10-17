f2612440684832548393924dccc9b7356af9a373
Islam Mokrane <islammkr01@gmail.com>
2024-10-17 16:06:41 +0200

    chore: remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -20,7 +18,6 @@ import { MongooseModule } from '@nestjs/mongoose';
         AuthModule,
         UsersModule,
     ],
-    controllers: [AppController],
-    providers: [AppService, { provide: 'APP_GUARD', useClass: JwtAuthGuard }],
+    providers: [{ provide: 'APP_GUARD', useClass: JwtAuthGuard }],
 })
 export class AppModule {}
